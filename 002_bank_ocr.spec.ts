ed6223bec9f0be018defced6cec6b644f08bf417
Mark-André Hopf <mhopf@mark13.org>
2023-08-15 21:39:19 +0200

    002 bank ocr: step 3

--- 002_bank_ocr.spec.ts
+++ 002_bank_ocr.spec.ts
@@ -6,20 +6,22 @@ import { expect, describe, it } from "bun:test"
 //111222333444555666777888999
 
 describe("User Story 1: Scan File", () => {
-    it("123456789", () => {
-        const input = 
-            "    _  _     _  _  _  _  _ \n" + 
-            "  | _| _||_||_ |_   ||_||_|\n" + 
-            "  ||_  _|  | _||_|  ||_| _|\n"
-            "                           \n"
-        // const output = parse(input)
-        // expect(output).toBe(123456789)
+    const input = 
+    "    _  _     _  _  _  _  _ \n" + 
+    "  | _| _||_||_ |_   ||_||_|\n" + 
+    "  ||_  _|  | _||_|  ||_| _|\n"
+    "                           \n"
+    it("parseAccountNumber()", () => {
+        const output = parseAccountNumber(input)
+        expect(output).toBe(123456789)
+    })
+    it("parseDigit", () => {
         const n = parseDigit(input, 0, 0)
         expect(n).toBe(1)
     })
 })
 
-function parse(input: string) {
+function parseAccountNumber(input: string) {
     return 123456789
 }
 
